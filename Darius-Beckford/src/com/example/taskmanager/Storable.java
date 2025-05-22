27fb6fbb53e7a5595a620b9fdaa3d43f62b010a1
Raydar35 <d.beckford28@ncf.edu>
2025-05-21 21:41:41 -0400

    Delete Darius-Beckford directory

--- Darius-Beckford/src/com/example/taskmanager/Storable.java
+++
@@ -1,6 +0,0 @@
-package com.example.taskmanager;
-
-public interface Storable {
-    void saveToFile(String filename);
-    void loadFromFile(String filename);
-}
--- Darius-Beckford/src/week6/BaseCipher.java
+++
@@ -1,6 +0,0 @@
-package week6;
-
-public interface BaseCipher {
-    String encrypt(String message);
-    String decrypt(String message);
-}
